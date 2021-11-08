2b173abe359e7c940a26fc6afb00dcb728bdf05d
Andrew Trice <amtrice@us.ibm.com>
2021-11-08 16:36:15 -0500

    added a debug statement for vpn creation
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-vpn.sh
+++ scripts/create-vpn.sh
@@ -4,6 +4,20 @@ ibmcloud config --check-version=false
 
 ibmcloud login -r "${REGION}" -g "${RESOURCE_GROUP}" --apikey "${IBMCLOUD_API_KEY}" || exit 1
 
+echo "ibmcloud is vpn-server-create  \
+  --name \"${VPN_SERVER}\"  \
+  --subnet \"${SUBNET_ID}\"  \
+  --cert \"${SERVER_CERT_CRN}\"  \
+  --client-ca \"${CLIENT_CERT_CRN}\"  \
+  --client-ip-pool \"${VPNCLIENT_IP}\"  \
+  --client-dns \"${CLIENT_DNS}\"  \
+  --client-auth-methods \"${AUTH_METHOD}\"  \
+  --sg \"${SECGRP_ID}\"  \
+  --protocol \"${VPN_PROTO}\"  \
+  --port \"${VPN_PORT}\"  \
+  --enable-split-tunnel \"${SPLIT_TUNNEL}\"  \
+  --client-idle-timeout \"${IDLE_TIMEOUT}\" "
+
 ibmcloud is vpn-server-create  \
   --name "${VPN_SERVER}"  \
   --subnet "${SUBNET_ID}"  \
